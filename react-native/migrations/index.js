01e290743ad49358820927462eff3b666983ac84
Valter Balegas <balegas@live.com>
2022-10-18 18:40:16 +0100

    Added new dependencies for existing demos and modified migrations files

--- react-native/migrations/index.js
+++ react-native/migrations/index.js
@@ -5,7 +5,7 @@ export const data = {
         'CREATE TABLE IF NOT EXISTS _electric_oplog (\n  rowid INTEGER PRIMARY KEY AUTOINCREMENT,\n  namespace String NOT NULL,\n  tablename String NOT NULL,\n  optype String NOT NULL,\n  primaryKey String NOT NULL,\n  newRow String,\n  oldRow String,\n  timestamp TEXT\n);',
         'CREATE TABLE IF NOT EXISTS _electric_meta (\n  key TEXT,\n  value BLOB\n);',
         'CREATE TABLE IF NOT EXISTS _electric_migrations (\n  id INTEGER PRIMARY KEY AUTOINCREMENT,\n  name TEXT NOT NULL UNIQUE,\n  sha256 TEXT NOT NULL,\n  applied_at TEXT NOT NULL\n);',
-        "INSERT INTO _electric_meta (key, value) VALUES ('compensations', '0'), ('lastAckdRowId','0'), ('lastSentRowId', '0'), ('lsn', x'30');",
+        "INSERT INTO _electric_meta (key, value) VALUES ('compensations', '0'), ('lastAckdRowId','0'), ('lastSentRowId', '0'), ('lsn', 'MA==');",
         'DROP TABLE IF EXISTS _electric_trigger_settings;',
         '\nCREATE TABLE _electric_trigger_settings(tablename STRING PRIMARY KEY, flag INTEGER);',
       ],
