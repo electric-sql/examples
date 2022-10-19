98823c04e9ea29de39e048b56099da570eb9ae13
Valter Balegas <balegas@live.com>
2022-10-19 16:46:56 +0100

    Remove old examples

--- legacy-react-native/ios/ElectricSQLDemo/Migration.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS items (
-  value TEXT PRIMARY KEY
-);
--- web/worker.js
+++
@@ -1,4 +0,0 @@
-import { ElectricWorker } from "electric-sql/browser";
-import { data as migrationsData } from "./migrations";
-
-ElectricWorker.start(self, migrationsData);
