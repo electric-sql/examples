23a5c734ddb07c701e5bcc5c48b08d474a035ffd
Garry Hill <garry@magnetised.net>
2022-10-31 11:29:26 +0000

    fix browser example configuration

--- web/src/worker.js
+++ web/src/worker.js
@@ -2,4 +2,4 @@ import { ElectricWorker } from 'electric-sql/browser'
 import { data } from "../migrations";
 import config from "../electric-config";
 
-ElectricWorker.start(self, { config, migrations: data.migrations });
+ElectricWorker.start(self, { ...config, migrations: data.migrations });
